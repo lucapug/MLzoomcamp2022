4cc1c00c418cf44ed48cc6d17862848b16a43825
Luca Pugliese <lucapug@gmail.com>
2022-11-26 11:44:02 +0100

    API gateway and web deployment

--- 09-serverless/test.py
+++ 09-serverless/test.py
@@ -1,6 +1,11 @@
 import requests
 
+#local testing
 url = 'http://localhost:8080/2015-03-31/functions/function/invocations'
+
+#web testing. lambda function exposed via api gateway
+#url = FOR SECURITY REASONS \
+# copy this part from inside aws api gateway and add '/predict'
 
 data = {'url': 'http://bit.ly/mlbookcamp-pants'}
 
